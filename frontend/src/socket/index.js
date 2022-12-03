5fa286b6bc2f38ba9ec394604aad75d11b225de6
Naman15032001 <namanjain15032001@gmail.com>
2022-12-03 18:27:30 +0530

    deploy

--- frontend/src/socket/index.js
+++ frontend/src/socket/index.js
@@ -1,14 +1,13 @@
 import { io } from 'socket.io-client';
 
-export const socketInit = () => {
-
+const socketInit = () => {
     const options = {
         'force new connection': true,
-        reconnectionAttempt: 'Infinity',
+        reconnectionAttempts: 'Infinity',
         timeout: 10000,
         transports: ['websocket'],
+    };
+    return io(process.env.REACT_APP_SOCKET_SERVER_URL, options);
+};
 
-    }
-
-    return io('http://localhost:5500',options)
-}
+export default socketInit;
